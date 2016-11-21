35c17c0bb1be27b479b6151ed8360c83e6d5650b
Frédéric Bentouati <frederic.bentouati@outscale.com>
2016-11-21 22:51:07 +0100

    Change endpoint declaration

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ with open('.config','r') as config:
   AK, SK = config.read().rstrip().split('\n')
 
 EC2endpoint = "https://fcu.eu-west-2.outscale.com"
-ec2 = boto3.resource(service_name='ec2', region_name='eu-west-2', endpoint_url='https://fcu.eu-west-2.outscale.com', aws_access_key_id=AK, aws_secret_access_key=SK)
+ec2 = boto3.resource(service_name='ec2', region_name='eu-west-2', endpoint_url=EC2endpoint, aws_access_key_id=AK, aws_secret_access_key=SK)
 
 def create_vpc():
   cidr_vpc = '192.168.0.0/24'
