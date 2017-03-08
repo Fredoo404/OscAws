0dc26cfe9cdef6c5381f6fdf2a0b196a997b48b4
Frédéric BENTOUATI <fredoo404@gmail.com>
2017-03-09 00:31:04 +0100

    Add bastion and allocation eip on this instance

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import boto3
 
 #default_omi = 'ami-d4f2f835' # Centos 7 eu-west-2
 default_omi = 'ami-6058c4d0'  # Centos 7 us-west-1
+key_pair = 'osc'
 
 with open('.config','r') as config:
   AK, SK = config.read().rstrip().split('\n')
@@ -14,9 +15,15 @@ EC2endpoint = "https://fcu.us-west-1.outscale.com"
 #EC2region = "eu-west-2"
 EC2region = "us-west-1"
 
+cli = boto3.client(service_name='ec2', region_name=EC2region, endpoint_url=EC2endpoint, aws_access_key_id=AK, aws_secret_access_key=SK)
 ec2 = boto3.resource(service_name='ec2', region_name=EC2region, endpoint_url=EC2endpoint, aws_access_key_id=AK, aws_secret_access_key=SK)
 
 def create_vpc():
+  # Create a VPC with :
+  # - 1 public subnet
+  # - 1 private subnet
+  # - 1 one internet gateway.
+  # - 1 instance "bastion" reachable from internet thanks to eip.
   cidr_vpc = '192.168.0.0/24'
   cidr_subnet = ['192.168.0.0/25','192.168.0.128/25']
   vpc_tags = {'Key':'Name','Value':'foobar'}
@@ -26,11 +33,16 @@ def create_vpc():
     subnet = vpc.create_subnet(CidrBlock=i)
     if i == cidr_subnet[0]:
         subnet_tags = {'Key':'Name','Value':'public'}
+        subnet.create_tags(Tags=[subnet_tags])
+        bastion = subnet.create_instances(ImageId=default_omi, InstanceType='t1.micro', KeyName=key_pair, MinCount=1, MaxCount=1)
+        bastion_tags = {'Key':'Name','Value':'bastion'}
+        bastion[0].create_tags(Tags=[bastion_tags])
     else:
         subnet_tags = {'Key':'Name','Value':'private'}
-    subnet.create_tags(Tags=[subnet_tags])
+        subnet.create_tags(Tags=[subnet_tags])
   gateway = ec2.create_internet_gateway()
   gateway.attach_to_vpc(VpcId=vpc.id)
-  bastion = ec2.create_instances(ImageId=default_omi, InstanceType='t1.micro', MinCount=1, MaxCount=1)
+  bastion_ip = cli.allocate_address(Domain='vpc')
+  cli.associate_address(InstanceId=bastion[0].id, AllocationId=bastion_ip['AllocationId'])
 
 create_vpc()
