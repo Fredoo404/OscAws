bc8fb50188f67d74a22e18faf86045d3900b6497
Frédéric BENTOUATI <fredoo404@gmail.com>
2017-05-29 21:31:05 +0200

    Move create_instance from main.py to create_instance.py

--- create_instance.py
+++ create_instance.py
@@ -1,25 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import boto3
+import common
 import time
 
-default_omi = 'ami-d4f2f835' # Centos 7 eu-west-2
-#default_omi = 'ami-6058c4d0'  # Centos 7 us-west-1
-key_pair = 'osc'
+AK, SK, AMI, KP, REGION = common.config()
+cli, ec2 = common.boto_connector('ec2')
 
-with open('.config','r') as config:
-  AK, SK = config.read().rstrip().split('\n')
-
-#EC2endpoint = "https://fcu.us-west-1.outscale.com"
-EC2endpoint = "https://fcu.eu-west-2.outscale.com"
-EC2region = "eu-west-2"
-#EC2region = "us-west-1"
-
-cli = boto3.client(service_name='ec2', region_name=EC2region, endpoint_url=EC2endpoint, aws_access_key_id=AK, aws_secret_access_key=SK)
-ec2 = boto3.resource(service_name='ec2', region_name=EC2region, endpoint_url=EC2endpoint, aws_access_key_id=AK, aws_secret_access_key=SK)
-
-def create_instance(config=None, InsType='t1.micro', Omi=default_omi, ebsSize=None):
+def create_instance(config=None, InsType='t1.micro', Omi=AMI, KeyPair=KP,ebsSize=None):
     """
         Function which will create an instance with parameters configured.
             # Create default instance 
@@ -37,10 +25,13 @@ def create_instance(config=None, InsType='t1.micro', Omi=default_omi, ebsSize=No
     else:
         userdata = ''
     instance_tags = {'Key':'Name','Value':'foobar'}
-    instance = ec2.create_instances(ImageId=Omi, InstanceType=InsType, KeyName=key_pair, UserData=userdata, MinCount=1, MaxCount=1)[0]
+    instance = ec2.create_instances(ImageId=Omi, InstanceType=InsType, KeyName=KeyPair, UserData=userdata, MinCount=1, MaxCount=1)[0]
     instance.create_tags(Tags=[instance_tags])
     if ebsSize != None:
         vol = ec2.create_volume(Size=ebsSize, AvailabilityZone=EC2region + "a")
 	while instance.state['Name'] != 'running':
 		instance.reload()
         instance.attach_volume(VolumeId=vol.id, Device="/dev/xvdb")
+
+if __name__ == "__main__":
+    create_instance()
